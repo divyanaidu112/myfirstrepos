3cf82425d1960c6a367387b2ad7b183d96e63826
divya naidu <divyapenumalla@gmail.com>
2020-01-27 14:03:52 +0530

    added calc function

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x,y):
+	"""add function"""
+	return x + y
+
+def substraction(x,y)
+	"""substraction"""
+	return x - y
+
+def multiply(x,y)
+	"""multiplication"""
+	return x * y
+
+def divide(x,y)
+	"""division"""
+	return x / y
